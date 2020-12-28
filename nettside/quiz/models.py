f4f7931dce001059bb0aeeaab19e29166cf8e329
Fredrik Borbe <fredrik.borbe@gmail.com>
2020-12-28 21:49:47 +0100

    Adda bruker modell og spørsmål+quizark

--- nettside/quiz/models.py
+++ nettside/quiz/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=500)
+    answer = models.CharField(max_length=100)
+
+class Quizark(models.Model):
+    title = models.CharField(max_length=100)
+    file = models.FileField()
+    question = models.ManyToManyField(Question)
+
