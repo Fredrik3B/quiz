4a1ad5c8ffd0974ef69987a6e6eae7b5da94f2eb
Nikolai <bus.nikolai@gmail.com>
2020-12-30 20:47:47 +0100

    push

--- nettside/accounts/forms.py
+++ nettside/accounts/forms.py
@@ -4,9 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class CreateUserForm(UserCreationForm):
-    username = forms.CharField(max_length=100)
-    password1 = forms.CharField(max_length=100)
-    password2 = forms.CharField(max_length=100)
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password1 = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Repeat password'}))
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2')
