d16cda752bc76a164fb760906e7cc5c092728bdc
Fredrik Borbe <fredrik.borbe@gmail.com>
2020-12-31 15:20:06 +0100

    Lagde LogInForm med placeholder widget

--- nettside/accounts/forms.py
+++ nettside/accounts/forms.py
@@ -1,6 +1,6 @@
 
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User
 
 class CreateUserForm(UserCreationForm):
@@ -11,4 +11,12 @@ class CreateUserForm(UserCreationForm):
         model = User
         fields = ('username', 'password1', 'password2')
 
+class LogInForm(AuthenticationForm):
 
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Brukernavn'}),
+            'password': forms.TextInput(attrs={'placeholder': 'Passord'}),
+        }
