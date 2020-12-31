72fe7e64aa88b38887a2ca072754a6f245a8ff45
Fredrik Borbe <fredrik.borbe@gmail.com>
2020-12-31 15:52:06 +0100

    Fikset eksta attributter i LogInForm

--- nettside/accounts/forms.py
+++ nettside/accounts/forms.py
@@ -12,11 +12,9 @@ class CreateUserForm(UserCreationForm):
         fields = ('username', 'password1', 'password2')
 
 class LogInForm(AuthenticationForm):
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'autofocus': True, 'placeholder': 'Brukernavn'}))
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'autocomplete': 'current-password','placeholder': 'Passord'}))
 
     class Meta:
         model = User
         fields = ('username', 'password')
-        widgets = {
-            'username': forms.TextInput(attrs={'placeholder': 'Brukernavn'}),
-            'password': forms.TextInput(attrs={'placeholder': 'Passord'}),
-        }
