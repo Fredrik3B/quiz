b8fd6e73c238d011d7049404bfeaed56be3dee3b
Fredrik Borbe <fredrik.borbe@gmail.com>
2021-01-15 22:15:53 +0100

    javascript som finner alle svar knappene

--- nettside/quiz/static/js/quiz_api.js
+++ nettside/quiz/static/js/quiz_api.js
@@ -5,36 +5,41 @@ function validateResponse(response) {
     return response.json();
 }
 
-fetch("api/").then(validateResponse)
+function getQuestions() {
+    fetch("api/").then(validateResponse)
     // .then(d => console.log(d))
-    .then(data => {
-        const title = data.title;
-        console.log(title);
-        const questions = data.question;
-        console.log(questions);
+        .then(data => {
+            const title = data.title;
+            console.log(title);
+            const questions = data.question;
+            console.log(questions);
 
-        for (const question of questions) {
-            // var fragment = new DocumentFragment()
-            var questionCard = document.createElement("div");
-            questionCard.className = "quizcard";
-            var questexttionTitle = document.createElement("h3");
-            questexttionTitle.appendChild(document.createTextNode(question.question));
-            questionCard.appendChild(questexttionTitle);
+            for (const question of questions) {
+                // var fragment = new DocumentFragment()
+                var questionCard = document.createElement("div");
+                questionCard.className = "quizcard";
+                var questexttionTitle = document.createElement("h3");
+                questexttionTitle.appendChild(document.createTextNode(question.question));
+                questionCard.appendChild(questexttionTitle);
 
-            var question_input = document.createElement("input")
-            question_input.type = "text"
-            question_input.className = "anwerinput";
-            questionCard.appendChild(question_input);
+                var question_input = document.createElement("input")
+                question_input.type = "text"
+                question_input.className = "answerinput";
+                questionCard.appendChild(question_input);
 
-            var question_submit = document.createElement("button")
-            question_submit.type = "submit"
-            question_submit.appendChild(document.createTextNode("Sjekk Svar"));
+                var question_submit = document.createElement("button");
+                // question_submit.type = "submit"
+                question_submit.appendChild(document.createTextNode("Sjekk Svar"));
 
-            document.getElementById("container")
-                .appendChild(questionCard)
-                .appendChild(question_submit);
-
-        }
-    })
+                document.getElementById("container")
+                    .appendChild(questionCard)
+                    .appendChild(question_submit);
 
 
+            };
+            var container = document.getElementById("container");
+            container.querySelectorAll("button").forEach(button => console.log(button));
+        })
+
+};
+getQuestions();
