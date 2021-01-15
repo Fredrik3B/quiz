d1c78b0f73584d63bd2020e56a0558b4ee99a179
Fredrik Borbe <fredrik.borbe@gmail.com>
2021-01-15 21:25:36 +0100

    Bruker api til å lage spørsmål i play_quiz.html

--- nettside/quiz/static/js/quiz_api.js
+++ nettside/quiz/static/js/quiz_api.js
@@ -1,11 +1,40 @@
 function validateResponse(response) {
-      if (!response.ok) {
-              throw Error(response.statusText);
-            }
-      return response.json();
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response.json();
 }
 
-fetch("api/").then(validateResponse).then(data => console.log(data)
-)
+fetch("api/").then(validateResponse)
+    // .then(d => console.log(d))
+    .then(data => {
+        const title = data.title;
+        console.log(title);
+        const questions = data.question;
+        console.log(questions);
+
+        for (const question of questions) {
+            // var fragment = new DocumentFragment()
+            var questionCard = document.createElement("div");
+            questionCard.className = "quizcard";
+            var questexttionTitle = document.createElement("h3");
+            questexttionTitle.appendChild(document.createTextNode(question.question));
+            questionCard.appendChild(questexttionTitle);
+
+            var question_input = document.createElement("input")
+            question_input.type = "text"
+            question_input.className = "anwerinput";
+            questionCard.appendChild(question_input);
+
+            var question_submit = document.createElement("button")
+            question_submit.type = "submit"
+            question_submit.appendChild(document.createTextNode("Sjekk Svar"));
+
+            document.getElementById("container")
+                .appendChild(questionCard)
+                .appendChild(question_submit);
+
+        }
+    })
 
 
