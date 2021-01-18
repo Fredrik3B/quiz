f7f7738c3e42c080c09a251f3e8e61aaabe795df
Fredrik Borbe <fredrik.borbe@gmail.com>
2021-01-18 20:27:37 +0100

    cleaner quiz_api.js

--- nettside/quiz/static/js/quiz_api.js
+++ nettside/quiz/static/js/quiz_api.js
@@ -5,41 +5,43 @@ function validateResponse(response) {
     return response.json();
 }
 
+function makeQuestions(data) {
+    const title = data.title;
+    const questions = data.question;
+
+    for (const question of questions) {
+        var questionCard = document.createElement("div");
+        questionCard.className = "quizcard";
+        var questexttionTitle = document.createElement("h3");
+        questexttionTitle.appendChild(document.createTextNode(question.question));
+        questionCard.appendChild(questexttionTitle);
+
+        var question_input = document.createElement("input")
+        question_input.type = "text"
+        question_input.className = "answerinput";
+        questionCard.appendChild(question_input);
+
+        var question_submit = document.createElement("button");
+        question_submit.appendChild(document.createTextNode("Sjekk Svar"));
+
+        document.getElementById("container")
+            .appendChild(questionCard)
+            .appendChild(question_submit);
+
+
+    };
+}
+
+function checkButttonsInit() {
+    var container = document.getElementById("container");
+    container.querySelectorAll("button").forEach(button => console.log(button));
+}
+
 function getQuestions() {
-    fetch("api/").then(validateResponse)
-    // .then(d => console.log(d))
-        .then(data => {
-            const title = data.title;
-            console.log(title);
-            const questions = data.question;
-            console.log(questions);
-
-            for (const question of questions) {
-                // var fragment = new DocumentFragment()
-                var questionCard = document.createElement("div");
-                questionCard.className = "quizcard";
-                var questexttionTitle = document.createElement("h3");
-                questexttionTitle.appendChild(document.createTextNode(question.question));
-                questionCard.appendChild(questexttionTitle);
-
-                var question_input = document.createElement("input")
-                question_input.type = "text"
-                question_input.className = "answerinput";
-                questionCard.appendChild(question_input);
-
-                var question_submit = document.createElement("button");
-                // question_submit.type = "submit"
-                question_submit.appendChild(document.createTextNode("Sjekk Svar"));
-
-                document.getElementById("container")
-                    .appendChild(questionCard)
-                    .appendChild(question_submit);
-
-
-            };
-            var container = document.getElementById("container");
-            container.querySelectorAll("button").forEach(button => console.log(button));
-        })
+    fetch("api/")
+        .then(validateResponse)
+        .then(data => makeQuestions(data))
+        .then(checkButttonsInit)
 
 };
 getQuestions();
